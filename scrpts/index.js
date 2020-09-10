0e6505e5279d0c39e64d04513e2c391279fa48b7
estellaZhang <zhangyj@sino-bridge.com>
2020-09-10 10:11:06 +0800

    测试网站流量是否统计

---
+++ scrpts/index.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1374c13d875748fe372be4f10e12318a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
---
+++ source/tags/myTages.md
@@ -0,0 +1,6 @@
+---
+title: myTages
+date: 2020-09-09 10:34:07
+---
+
+## mytagslist
